63eab382cd29edb2089d5abfeb034a8a79803f29
Luckays <lukasbeloch@seznam.cz>
2026-05-22 14:45:38 +0200

    Add Nepomuk GitHub Pages viewer

---
+++ README_GITHUB_PAGES.txt
@@ -0,0 +1,22 @@
+GitHub Pages export for project Nepomuk
+
+How to publish
+1. Create a new GitHub repository
+2. Upload the contents of this folder to the repository root
+3. Push to branch main
+4. In GitHub repository settings enable Pages with GitHub Actions
+5. The included workflow will deploy the site automatically
+
+Entry point
+- index.html
+
+Included
+- 2D viewer
+- 3D Potree viewer
+- panoramas
+- cloud.ply
+- Potree octree data
+
+Important
+- This is a public static site
+- Anyone who can open the site can also fetch the point cloud files in the browser network layer
---
+++ potree_scene/pointcloud_v17/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 42589486,
+    "boundingBox": {
+        "lx": -35.04560089111328,
+        "ly": 0.15549996495246888,
+        "lz": 0.7619001269340515,
+        "ux": 4.486399108886722,
+        "uy": 39.68749996495247,
+        "uz": 40.293900126934058
+    },
+    "tightBoundingBox": {
+        "lx": -35.04560089111328,
+        "ly": 0.15549996495246888,
+        "lz": 0.7619001269340515,
+        "ux": 4.486399108886722,
+        "uy": 7.190499964952469,
+        "uz": 7.041900126934052
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.34235715866088869,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
