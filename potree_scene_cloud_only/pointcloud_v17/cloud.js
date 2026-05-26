cc40d0510c06ad5322df795666a4bde5b89b9aae
Luckays <lukasbeloch@seznam.cz>
2026-05-26 22:13:10 +0200

    Use separate cloud for standalone Potree view

---
+++ potree_scene_cloud_only/pointcloud_v17/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 73306817,
+    "boundingBox": {
+        "lx": -48.987403869628909,
+        "ly": 54.48451232910156,
+        "lz": -0.7585043907165527,
+        "ux": -9.246403869628907,
+        "uy": 94.22551232910156,
+        "uz": 38.98249560928345
+    },
+    "tightBoundingBox": {
+        "lx": -48.987403869628909,
+        "ly": 54.48451232910156,
+        "lz": -0.7585043907165527,
+        "ux": -42.064403869628907,
+        "uy": 94.22551232910156,
+        "uz": 5.634495609283447
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.34416714310646059,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
